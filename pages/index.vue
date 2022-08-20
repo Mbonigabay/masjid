5d03eab6a23929b872bf2e100bb5d7c2d8ef6e32
Yusuf Mbonigaba <mbonigabay@gmail.com>
2022-08-20 16:15:58 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,386 @@
+<template>
+  <div class="container">
+    <header
+      class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
+    >
+      <a
+        href="/"
+        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+      >
+        <i class="fa-solid fa-mosque"></i>
+        <span class="fs-4">masjid.rw</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark">Mosques</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark">About us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark">Services</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark">Program</a>
+        </li>
+      </ul>
+    </header>
+    <section id="intro">
+      <div class="my-5">
+        <div
+          class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg"
+        >
+          <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 class="display-4 fw-bold lh-1">
+              Border hero with cropped image and shadows
+            </h1>
+            <p class="lead">
+              Quickly design and customize responsive mobile-first sites with
+              Bootstrap, the world’s most popular front-end open source toolkit,
+              featuring Sass variables and mixins, responsive grid system,
+              extensive prebuilt components, and powerful JavaScript plugins.
+            </p>
+          </div>
+          <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <img
+              class="rounded-lg-3"
+              src="https://source.unsplash.com/random/1920x1080/?wallpaper,landscape"
+              alt=""
+              width="720"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="announcement">
+      <h3>Announcement</h3>
+      <div class="row mb-2">
+        <div class="col-md-6">
+          <div
+            class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
+          >
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-primary">World</strong>
+              <h3 class="mb-0">Featured post</h3>
+              <div class="mb-1 text-muted">Nov 12</div>
+              <p class="card-text mb-auto">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <svg
+                class="bd-placeholder-img"
+                width="200"
+                height="250"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: Thumbnail"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c"></rect>
+                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div
+            class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
+          >
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <h3 class="mb-0">Post title</h3>
+              <div class="mb-1 text-muted">Nov 11</div>
+              <p class="mb-auto">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <svg
+                class="bd-placeholder-img"
+                width="200"
+                height="250"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: Thumbnail"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c"></rect>
+                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="services">
+      <div class="px-4 py-5" id="icon-grid">
+        <h3 class="pb-2 border-bottom">Services</h3>
+
+        <div
+          class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5"
+        >
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#bootstrap"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#cpu-fill"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#calendar3"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#home"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#speedometer2"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#toggles2"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#geo-fill"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg
+              class="bi text-muted flex-shrink-0 me-3"
+              width="1.75em"
+              height="1.75em"
+            >
+              <use xlink:href="#tools"></use>
+            </svg>
+            <div>
+              <h4 class="fw-bold mb-0">Featured title</h4>
+              <p>
+                Paragraph of text beneath the heading to explain the heading.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="programs">
+      <div class="px-4 py-5" id="custom-cards">
+        <h2 class="pb-2 border-bottom">Programs</h2>
+
+        <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+          <div class="col">
+            <div
+              class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style="background-image: url('unsplash-photo-1.jpg')"
+            >
+              <div
+                class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
+              >
+                <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Short title, long jacket
+                </h2>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                    <img
+                      src="https://github.com/twbs.png"
+                      alt="Bootstrap"
+                      width="32"
+                      height="32"
+                      class="rounded-circle border border-white"
+                    />
+                  </li>
+                  <li class="d-flex align-items-center me-3">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#geo-fill"></use>
+                    </svg>
+                    <small>Earth</small>
+                  </li>
+                  <li class="d-flex align-items-center">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#calendar3"></use>
+                    </svg>
+                    <small>3d</small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div
+              class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style="background-image: url('unsplash-photo-2.jpg')"
+            >
+              <div
+                class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1"
+              >
+                <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Much longer title that wraps to multiple lines
+                </h2>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                    <img
+                      src="https://github.com/twbs.png"
+                      alt="Bootstrap"
+                      width="32"
+                      height="32"
+                      class="rounded-circle border border-white"
+                    />
+                  </li>
+                  <li class="d-flex align-items-center me-3">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#geo-fill"></use>
+                    </svg>
+                    <small>Pakistan</small>
+                  </li>
+                  <li class="d-flex align-items-center">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#calendar3"></use>
+                    </svg>
+                    <small>4d</small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div
+              class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style="background-image: url('unsplash-photo-3.jpg')"
+            >
+              <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Another longer title belongs here
+                </h2>
+                <ul class="d-flex list-unstyled mt-auto">
+                  <li class="me-auto">
+                    <img
+                      src="https://github.com/twbs.png"
+                      alt="Bootstrap"
+                      width="32"
+                      height="32"
+                      class="rounded-circle border border-white"
+                    />
+                  </li>
+                  <li class="d-flex align-items-center me-3">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#geo-fill"></use>
+                    </svg>
+                    <small>California</small>
+                  </li>
+                  <li class="d-flex align-items-center">
+                    <svg class="bi me-2" width="1em" height="1em">
+                      <use xlink:href="#calendar3"></use>
+                    </svg>
+                    <small>5d</small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="teams">
+      <h3>Our team</h3>
+    </section>
+  </div>
+</template>
+<script setup>
+const route = useRoute();
+const masjidSlug = !!route.query.masjid ? route.query.masjid : "rmc";
+console.log(masjidSlug);
+</script>
